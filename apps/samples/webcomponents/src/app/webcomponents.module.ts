4a94957e7af63c4b96e31709651b82e0357d49bb
BLaZeKiLL <blazeingamer10@gmail.com>
2020-11-13 19:24:52 +0530

    angular elements configured for wc samples

--- apps/samples/webcomponents/src/app/webcomponents.module.ts
+++ apps/samples/webcomponents/src/app/webcomponents.module.ts
@@ -1,12 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { DoBootstrap, Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
-  declarations: [HelloWorldComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [],
+  declarations: [
+    HelloWorldComponent
+  ],
+  imports: [
+    BrowserModule
+  ]
 })
-export class WebComponentsModule {}
+export class WebComponentsModule implements DoBootstrap {
+
+  constructor(private injector : Injector) {}
+
+  ngDoBootstrap(): void {
+    customElements.define('wc-hello-world', createCustomElement(HelloWorldComponent, {injector: this.injector}));
+  }
+
+}
