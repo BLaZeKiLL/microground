338897f7a3793448c5c49b984f9e6096b4cc8809
BLaZeKiLL <blazeingamer10@gmail.com>
2020-12-08 12:15:44 +0530

    storybook added to samples-wc-angular

---
+++ apps/samples/wc-angular/src/app/hello-world/hello-world.component.stories.ts
@@ -0,0 +1,16 @@
+import { text, number, boolean } from '@storybook/addon-knobs';
+import { HelloWorldComponent } from './hello-world.component';
+
+export default {
+  title: 'HelloWorldComponent'
+}
+
+export const primary = () => ({
+  moduleMetadata: {
+    imports: []
+  },
+  component: HelloWorldComponent,
+  props: {
+    message: text('message', ''),
+  }
+})
